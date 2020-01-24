3ebe58c01a4fcd453fac795fce22beae3895bc63
DahalPrakash <53021286+DahalPrakash@users.noreply.github.com>
2020-01-24 22:56:52 +0545

    ModelRelationship Completed with Screenshots

--- CollegeManagement/ClzManApp/models.py
+++ CollegeManagement/ClzManApp/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Result(models.Model):
-    ModuleName=models.CharField(max_length=30)
-    result=models.CharField(max_length=50)
     StudentName = models.CharField(max_length=50)
+    result=models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.StudentName} is {self.result} in {self.ModuleName}"
+        return f"{self.StudentName} is {self.result}"
 
 class Teachers(models.Model):
     TeacherName=models.CharField(max_length=40)
@@ -20,7 +19,7 @@ class Teachers(models.Model):
 class Class(models.Model):
     ClassName = models.CharField(max_length=128)
     StudentGroupName = models.CharField(max_length=40)
-    teacher_class = models.ForeignKey(Teachers, on_delete=models.CASCADE, related_name='classes')
+    teachers_class = models.ForeignKey(Teachers, on_delete=models.CASCADE, related_name='classes')
 
     def __str__(self):
         return self.ClassName
@@ -35,11 +34,12 @@ class Students(models.Model):
     def __str__(self):
         return self.StudentName
     
+    
 
 class Modules(models.Model):
     ModuleName=models.CharField(max_length=30)
-    studentID_modules=models.ManyToManyField(Students, related_name='study_modules')
-    teacherID_modules=models.ManyToManyField(Teachers,  related_name='teach_modules')
+    students_modules=models.ManyToManyField(Students, related_name='study_modules')
+    teachers_modules=models.ManyToManyField(Teachers,  related_name='teach_modules')
 
     def __str__(self):
         return self.ModuleName
